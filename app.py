d2f00b022d422a5e1e5fd975b4f30ef88b987684
girish c <girishcx@gmail.com>
2025-09-06 15:48:34 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -228,5 +228,5 @@ def get_sample_country_data(country_name):
 
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
